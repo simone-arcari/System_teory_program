76c9808545b8735871813b97e2bb6733d5483f9b
Simone Arcari <simonearcari@outlook.it>
2023-04-18 11:37:38 +0200

    fd

--- Progetti Tirocinio/Tirocinio 2/compito2.m
+++ Progetti Tirocinio/Tirocinio 2/compito2.m
@@ -8,22 +8,22 @@ gamma2 = 2;
 A = [0, 1 ; gamma1, gamma2];
 B = [0; 1];
 
-sampling_time = 1;
-max_time = 3;
+sampling_time = 0.5;
+max_time = 50;
 t = 0:sampling_time:max_time;
 d_tau = 0.1;
 
-G11 = zeros(1,max_time+1);
+G11 = zeros(1, 1+(max_time/sampling_time));
 
 for i = 1:1:max_time+1
     tau = 0:d_tau:t(i);
     G = zeros(2,2);
 
-    for j = 1:1:t(i)+1
+    for j = 1:1:(t(i)/sampling_time)+1
         G = G + expm(-A*tau(j))*B*B'*expm(-A'*tau(j));
         G11(i) = G(1,1);
     end
 end
 
 figure(1)
-plot(t, G11)
+plot(t, G11) 
