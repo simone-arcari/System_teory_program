e61bafa45a5a889069bb0d0d174217bf7e6fc023
Valeria Villano <vvillano15@gmail.com>
2023-06-26 12:14:28 +0200

    punto 2 iniziato

---
+++ Progetti Tirocinio/Tirocinio 2/punto2.m
@@ -0,0 +1,8 @@
+%%Scegliere un tempo t_bar (ci piace 10 sec) per ognuno dei 4 casi gli
+%%diamo un ingresso e lo simuliamo con Lsim (non facciamo nessun calcolo)
+%%Ricordarsi di capire come funziona Lsim
+%%Ricordarsi di salvare il valore di t_bar scelto
+%%Salvarsi i grafici prodotti dopo questa cosa
+%%Gli ingressi dobbiamo sceglierli in maniera variegata in base anche al
+%%caso in esame (stabili/instabili)
+%%Basa questo è quello che dobbiamo fare
